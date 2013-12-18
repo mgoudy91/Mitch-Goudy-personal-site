a24ad9af94cdc2d273780f256033a62de9cc4772
Mitch Goudy <mgoudy91@gmail.com>
2013-12-18 15:03:02 -0700

    New scrolling / content setup, looks awesome

--- res/js/page.js
+++ res/js/page.js
@@ -1,19 +1,41 @@
-/* On Ready*/
+// On ready
 $(document).ready(function() {
+	resize();
 	$('#nav a').first().addClass('selected');
 
 	$('#nav a').click(function() {
+
+		// Scroll animate to the top no matter what since it looks cool
+		$("#container").animate({ scrollTop: 0 }, "slow");
+
+		// If page selected isn't the one we're on
 		if(!$(this).hasClass('selected')) {
+
+			// Update selected data
 			$('#nav a').removeClass('selected');
 			$(this).addClass('selected');
+			var selectionId = $(this).attr('id');
+
+			// Fade content out and in
+			$('#content').fadeOut('medium', function() {
+				$('.page').css('display', 'none');
+				$('.page#'+selectionId).css('display', 'block');
+				$('#content').fadeIn('medium');
+			});
 		}
 
-		var selectionId = $(this).attr('id');
-
-		$('#content').fadeOut('medium', function() {
-			$('.page').css('display', 'none');
-			$('.page#'+selectionId).css('display', 'block');
-			$('#content').fadeIn('medium');
-		});
+		
 	});
 });
+
+// On Resize
+$( window ).resize(function() {
+  resize();
+});
+
+// Resize scrollbars on height change
+function resize() {
+	var height = $('body').height() - 75;
+	console.log(height);
+	$('#container').height(height + 'px');
+};
